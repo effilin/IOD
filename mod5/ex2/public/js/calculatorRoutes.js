a23a5c10e2a42d4505efbb51e0e74c90eb503fc2
effilin <effienummer@gmail.com>
2024-06-08 18:09:28 -0400

    two done

--- mod5/ex2/public/js/calculatorRoutes.js
+++ mod5/ex2/public/js/calculatorRoutes.js
@@ -11,7 +11,42 @@ router.get('/add', (req, res) => {
     console.log(sum)
     res.status(200)
     res.json({result:sum})
+})
 
+router.get('/subtract', (req, res) => {
+    console.log(req.query);
+    console.log(req.url);
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 - number2;
+    console.log(`number1: ${number1} number2: ${number2}`);
+    console.log(sum)
+    res.status(200)
+    res.json({result:sum})
+})
+
+router.get('/divide', (req, res) => {
+    console.log(req.query);
+    console.log(req.url);
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 / number2;
+    console.log(`number1: ${number1} number2: ${number2}`);
+    console.log(sum)
+    res.status(200)
+    res.json({result:sum})
+})
+
+router.get('/multiply', (req, res) => {
+    console.log(req.query);
+    console.log(req.url);
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = number1 * number2;
+    console.log(`number1: ${number1} number2: ${number2}`);
+    console.log(sum)
+    res.status(200)
+    res.json({result:sum})
 })
 
 export default router
