5744f935ef7787eedd8ea5db465f630ec74cee5c
effilin <effienummer@gmail.com>
2024-06-09 20:33:55 -0400

    updated ex5

--- mod5/ex5.c/controller/friendsController.js
+++ mod5/ex5.c/controller/friendsController.js
@@ -1,3 +1,5 @@
+import friends from "../models/friends.js";
+
 const filter =(req, res) => {
     console.log(req.query)
     let filterGender = req.query.gender;
@@ -28,3 +30,48 @@ const info =(req, res) => {
 
     res.json({userAgent, contentType, acceptHead});  
 }
+
+const idDynamic = (req, res) => {
+    let friendId = req.params.id; // 'id' here will be a value matching anything after the / in the request path
+    
+    // Modify this function to find and return the friend matching the given ID, or a 404 if not found
+    const idsCurrent = friends.map((person) => (person.id));
+    
+  if(idsCurrent.includes(parseInt(friendId))) { 
+    res.status(200).json({result: friends.filter((person) => person.id == friendId)})
+  } else {
+  res.status(404).json({result: `user ${friendId} not found`})
+}}
+
+const newFriend = (req, res) => {
+    let newFriend = req.body; // FIRST add this line to index.js: app.use(express.json());
+    console.log(newFriend) // 'body' will now be an object containing data sent via the request body
+
+    // we can add some validation here to make sure the new friend object matches the right pattern
+    if (!newFriend.name || !newFriend.gender) {
+        res.status(500).json({error: 'Friend object must contain a name and gender'});
+        return;
+    }
+    else if (!newFriend.id) {
+        newFriend.id = friends.length + 1; // generate an ID if one is not present
+    }
+
+    // if the new friend is valid, add them to the list and return the successfully added object
+    friends.push(newFriend)
+    res.status(200).json(newFriend)
+}
+
+const updateMe = (req, res) => {
+    let friendId = req.params.id;
+    let updatedFriend = req.body;
+
+
+    if(friends.includes(parseInt(friendId))) { 
+        //const result = friends.filter((person) => person.id == friendId)
+        friends.push(updatedFriend);
+
+    res.json({result: 'Updated friend with ID ' + friendId, data: updatedFriend})
+    }
+}
+
+export default { filter, info, idDynamic, newFriend, updateMe}
--- mod5/ex5.c/routes/friendRoutes.js
+++ mod5/ex5.c/routes/friendRoutes.js
@@ -31,45 +31,18 @@ router.get('/info', (req, res) => {
 
 // 3. Dynamic request param endpoint - get the friend matching the specific ID ie. /friends/3
 router.get('/:id', (req, res) => {
-    console.log(req.params)
-    let friendId = req.params.id; // 'id' here will be a value matching anything after the / in the request path
-    const friend = friends.find(friend => friend.id == friendId)
-    // Modify this function to find and return the friend matching the given ID, or a 404 if not found
- 
-  if(friendId === "/:id") { res.status(200).json ({result: friend})
- 
-  res.status(404).json({result: `user ${userId} not found`})
-}
+    friendController.idDynamic(req, res)
 })
+
 
 // a POST request with data sent in the body of the request, representing a new friend to add to our list
 router.post('/', (req, res) => {
-    let newFriend = req.body; // FIRST add this line to index.js: app.use(express.json());
-    console.log(newFriend) // 'body' will now be an object containing data sent via the request body
-
-    // we can add some validation here to make sure the new friend object matches the right pattern
-    if (!newFriend.name || !newFriend.gender) {
-        res.status(500).json({error: 'Friend object must contain a name and gender'});
-        return;
-    }
-    else if (!newFriend.id) {
-        newFriend.id = friends.length + 1; // generate an ID if one is not present
-    }
-
-    // if the new friend is valid, add them to the list and return the successfully added object
-    friends.push(newFriend)
-    res.status(200).json(newFriend)
+    friendController.newFriend(req, res)
 })
 
 // 4. Complete this new route for a PUT request which will update data for an existing friend
 router.put('/:id', (req, res) => {
-    let friendId = req.params.id;
-    let updatedFriend = req.body;
-
-    // Replace the old friend data for friendId with the new data from updatedFriend
-
-    // Modify this response with the updated friend, or a 404 if not found
-    res.json({result: 'Updated friend with ID ' + friendId, data: updatedFriend})
+    friendController.updateMe(req, res)
 })
 
 export default router;
