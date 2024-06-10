5dedf4c16b5cdbcc675f4f64dff29533733e14b9
effilin <effienummer@gmail.com>
2024-06-09 22:57:43 -0400

    6 not working but basic syntax is there

--- mod5/ex6/app.test.js
+++ mod5/ex6/app.test.js
@@ -6,15 +6,49 @@ describe('Calculator Routes', () => {
 // generate some random numbers to test the calculator
    let number1 = Math.floor(Math.random() * 1_000_000);
    let number2 = Math.floor(Math.random() * 1_000_000);
-test('GET /calculator/add => sum of numbers', () => {
-  return request(app)
-.get(`/calculator/add?num1=${number1}&num2=${number2}`)
-.expect('Content-Type', /json/)
-.expect(200)
-.then((response) => {
-expect(response.body).toEqual({
+   test('GET /calculator/add => sum of numbers', () => {
+        return request(app)
+        .get(`/calculator/add?num1=${number1}&num2=${number2}`)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+        expect(response.body).toEqual({
 result: (number1 + number2)
-});
-});
-});
+   });
+   });
+   });
+    test('Get /calculator/subtract => sum of numbers', () => {
+         return request(app)
+        .get(`/calculator/subtract?=${number1}&num2=${number2}`)
+        .expect('Content-type', /json/)
+        .expect(200)
+        .then((response) => {
+        expect(response.body).toEqual({
+            result:(number1 +number2)
+        })
+    })
+})
+test('Get /calculator/multiply => sum of numbers', () => {
+    return request(app)
+   .get(`/calculator/multiply?=${number1}&num2=${number2}`)
+   .expect('Content-type', /json/)
+   .expect(200)
+   .then((response) => {
+   expect(response.body).toEqual({
+       result:(number1 +number2)
+   })
+})
+})
+test('Get /calculator/divide => sum of numbers', () => {
+    return request(app)
+   .get(`/calculator/divide?=${number1}&num2=${number2}`)
+   .expect('Content-type', /json/)
+   .expect(200)
+   .then((response) => {
+   expect(response.body).toEqual({
+       result:(number1 +number2)
+   })
+})
+})
+
 });
