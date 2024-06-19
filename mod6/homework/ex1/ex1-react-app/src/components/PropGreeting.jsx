acdc2ffd3b5a8295e47b5143506433ebdf0f64a7
Travis Nummer <effienummer@gmail.com>
2024-06-19 17:42:15 -0400

    ex1 done

--- mod6/homework/ex1/ex1-react-app/src/components/PropGreeting.jsx
+++ mod6/homework/ex1/ex1-react-app/src/components/PropGreeting.jsx
@@ -1,14 +1,21 @@
-function PropGreeting(props) {
-
-    const stringProps = JSON.stringify(props);
-
+function PropGreeting({person}) {
+   
+if (person.name) {
     return (
         <div className="GreetingParent componentBox">
-            <h5>Hello World!</h5>
-            <h3>{stringProps}</h3>
+            <h5>Hello {person.name}!</h5>
+            {person.children}
         </div>
     )
-
+} else {
+    return (
+    <div className="GreetingParent componentBox">
+            <h5>Hello World!</h5>
+            {person.children}
+        </div>
+    )
+}
+   
 };
 
 export default PropGreeting
