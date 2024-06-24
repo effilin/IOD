9bb63908a4122035716e8922126e669f432d2982
Travis Nummer <effienummer@gmail.com>
2024-06-24 15:00:33 -0400

    calculator working

---
+++ mod6/homework/lab6/src/App.jsx
@@ -0,0 +1,13 @@
+import Calculator from './components/calc'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Calculator/>
+    </>
+  )
+}
+
+export default App
---
+++ mod6/homework/lab6/src/components/calc.jsx
@@ -0,0 +1,108 @@
+import './calc.css'
+import { useState } from 'react';
+
+const Calculator = () => {
+
+const addition=(a, b) => ((a*10) +(b* 10))/10;
+const subtract=(a, b) => a-b;
+const multiply=(a, b) => a*b;
+const divide=(a, b) => a/b
+
+const [result, setResult] = useState('');
+const [inputData2, setInputData2] = useState('');
+const [operator, setOperator] = useState('');
+const [inputData, setInputData] = useState('');
+
+const makeValue = (e) => {
+    const newNum = inputData + e;
+    setInputData(newNum);
+};
+
+const addSign = () => {
+  setOperator('+')
+  setInputData2(inputData)
+  setInputData('')
+};
+
+
+const subSign = () => {
+    setOperator('-')
+  setInputData2(inputData)
+  setInputData('')
+  };
+
+const multSign = () => {
+    setOperator('*')
+  setInputData2(inputData)
+  setInputData('')
+  };
+
+const divSign = () => {
+    setOperator('/')
+    setInputData2(inputData)
+    setInputData('')
+  };
+
+const reset = () => {
+    setOperator('')
+    setInputData2('')
+    setInputData('')
+};
+
+const equals = () => {
+    console.log(operator);
+    if(operator === "+") {
+        console.log("add")
+        const result = inputData2 + inputData;
+    }else if (operator === "-") {
+        console.log("minus");
+    }else if ( operator === "*") {
+        console.log('multiply');
+    } else if (operator === "/") {
+        console.log('divide');
+    } else{
+        console.log('error');
+    }
+};
+
+    return (
+        <div>
+        <h1>.....Calculator.....</h1>
+
+        <div id="wrapper">
+
+            <div id="screen">
+                <p>{result}</p>
+                <p id="results1">{inputData2}</p>
+                <p id="operator">{operator}</p>
+                <p id="results2">{inputData}</p>
+                
+            </div>
+
+            <button className="numBtn number"  onClick={() => makeValue('1')}>1</button>
+            <button className="numBtn number" onClick={() => makeValue('2')}>2</button>
+            <button className="numBtn number" onClick={() => makeValue('3')}>3</button>
+            <button className="numBtn operator" onClick={addSign}>+</button>
+            <button className="numBtn number" onClick={() => makeValue('4')}>4</button>
+            <button className="numBtn number" onClick={() => makeValue('5')}>5</button>
+            <button className="numBtn number" onClick={() => makeValue('6')}>6</button>
+            <button className="numBtn operator" onClick={subSign}>-</button>
+            <button className="numBtn number" onClick={() => makeValue('7')}>7</button>
+            <button className="numBtn number" onClick={() => makeValue('8')}>8</button>
+            <button className="numBtn number" onClick={() => makeValue('9')}>9</button>
+            <button className="numBtn operator" onClick={multSign}>*</button>
+            <button className="numBtn number" onClick={() => makeValue('9')}>0</button>
+            <button className="numBtn operator" onClick={divSign}>/</button>
+                 
+            
+
+            <button id="submit" type="submit" onClick={equals}>Equals</button>
+            <button id="reset" type="reset" onClick={reset}>Reset</button>
+
+        </div>
+       </div>
+
+    );
+}
+
+export default Calculator
