2c1cef57c045845e648d79194563c12fe3c86f5d
effilin <effienummer@gmail.com>
2024-06-30 15:20:49 -0400

    lab2 part1 done

---
+++ mod7/homework/lab2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import BitcoinRates from './components/bitcoinRates'
+
+function App() {
+  
+
+  return (
+    <>
+      <BitcoinRates/>
+    </>
+  )
+}
+
+export default App
