dda3f4a0adb74312384bcff8676bb4d3b0430938
effilin <effienummer@gmail.com>
2024-07-10 21:01:10 -0400

    labs done

--- mod7/homework/lab2.b/src/components/bitcoinRatesReducer.js
+++ mod7/homework/lab2.b/src/components/bitcoinRatesReducer.js
@@ -1,13 +1,32 @@
 import { useReducer, useEffect } from "react";
 
+ const reducer = (state, action) => {
+    switch (action.type) {
+        case 'fetch_success' :
+            return {
+                loading: false,
+                bitcoinRate: action.payload,                                                                                                                                                         
+                error: null,
+                }
+        case "fetch_error" :
+            return {
+            loading: false,
+            bitcoinRate: null,                                                                                                                                                         
+            error: action.payload,
+            }
+        default:
+            return {...state, loading: false}
+    }
+ };
+
 
  export default function BitcoinReducer(currency) {
 
-    const [bitcoinResults, dispatch] = useReducer(reducer, {
+    const [state, dispatch] = useReducer(reducer, {
        loading: true,
-       bitcoinRate: null,                                                                                                                                                         
-        error: null
-    })
+       bitcoinRate: [],                                                                                                                                                         
+       error: null
+    });
 
     useEffect(() => {
         console.log('updating Bitcoin rate');
@@ -15,20 +34,21 @@ import { useReducer, useEffect } from "react";
         fetch(
            `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}`
         )
-        .then((response) => {
-            dispatch({ type: 'fetch_success', payload: response.json.bitcoin[currency.toLowerCase()]})
+        .then((response)=> response.json())
+        .then((data) => {
+            const rate = data.bitcoin[currency.toLowerCase()];
+            dispatch({ type: 'fetch_success', payload: rate})
         })
         .catch(error => {
             dispatch({type:"fetch_error", payload:error.message})
         })
     return () => {
         console.log('cleanup');
-    }
-    },[bitcoinResults]); 
+    };
+    },[currency]); 
 
-    console.log(bitcoinResults)
+    console.log(state)
 
-    return bitcoinResults;
+    return state;
 }
-
 
