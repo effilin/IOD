398d8e5f1052b717e361c613d319318126511515
effilin <effienummer@gmail.com>
2024-07-15 17:40:01 -0400

    flippin cards

--- mini-project-2/escapeApp/components/puzzle/cards.jsx
+++ mini-project-2/escapeApp/components/puzzle/cards.jsx
@@ -8,32 +8,45 @@ import { animated } from "@react-spring/web";
 export default function Cards() {
 
     const [flipped, setFlipped] = useState(Array(CardStock.length).fill(false));
+    
 
     const flipStyle = flipped.map((isFlipped) => 
     useSpring({
-        from:{ opacity: 1 , transform: `perspective(600px) rotateX(0deg)`},
+        from:{ opacity: 1 , transform: `perspective(600px) rotateY(0deg)`},
         to: isFlipped 
-            ? { opacity: 0 , transform: `perspective(600px) rotateX(180deg)`} 
-            : { opacity: 1 , transform: `perspective(600px) rotateX(0deg)`},
+            ? { opacity: 0 , transform: `perspective(600px) rotateY(180deg)`} 
+            : { opacity: 1 , transform: `perspective(600px) rotateY(0deg)`},
         config:{ mass: 5, tension: 500, friction: 80},
     }))
 
+    const backFlipStyle = flipped.map((isFlipped) =>
+    useSpring({
+            from:{ visibility: 'hidden', transform: `perspective(600px) rotateY(0deg)`},
+            to: isFlipped 
+                ? { visibility: 'visible', transform: `perspective(600px) rotateY(1800deg)`} 
+                : { visibility: 'hidden', transform: `perspective(600px) rotateY(0deg)` }, 
+            config:{ mass: 5, tension: 500, friction: 80},
+        })
+    )
+
+
+
     const handleClick = (index) => {
         const newFlipped = [...flipped];
-        newFlipped[index] = !newFlipped[index]
+         newFlipped[index] = !newFlipped[index];
+        
         setFlipped(newFlipped);
     };
 
 
     const myCards = CardStock.map((card, index) =>(
         <Grid item xs={2} key={card.key}>
-        <Card id={card.id} className="flipCard" >
+        <Card className="flipCard" >
             <div className="flip-inner">
-                <animated.div className="flip-Front" style={{...flipStyle[index], backgroundColor:card.color}} onClick={() => handleClick(index)}>
+                <animated.div className="flip-front" style={{...flipStyle[index], backgroundColor:card.color}} onClick={() => handleClick(index)}>
                    <Typography variant="h2">{card.id}</Typography>
-                   <button>Flip me</button>
                 </animated.div>
-                <animated.div style={{...flipStyle[index]}} className="flip-back">
+                <animated.div style={{...backFlipStyle[index]}} className="flip-back" onClick={() => handleClick(index)}>
                     <p>back</p>
                 </animated.div>
             </div>
